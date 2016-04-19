6a7fd239ad6e3237ea0ac6df652a057169f57cf5
Mohammad Afshar <ma2510@nyu.edu>
2016-04-19 01:17:54 -0400

    fixing data output for labels

--- src/data_reader.py
+++ src/data_reader.py
@@ -17,32 +17,26 @@ def main():
     if not os.path.exists(os.path.dirname(OUTPUT_PATH)):
         try:
             os.makedirs(os.path.dirname(OUTPUT_PATH))
-        except OSError as exc: # Guard against race condition
+        except OSError as exc: # guard against race condition
             if exc.errno != errno.EEXIST:
                 raise
     files = glob.glob(INPUT_PATH + "*.jpg")
     output_fh = open(OUTPUT_PATH + "labels.txt", "w")
-    output_fh.write("suhhhh dude")
-    return
     class_number = 0
     prev = None
     curr = None
-    counter = 0
     # looping through the images
     for file in files:
         filename = file.split("/")[-1]
         curr = int(filename.split(".")[0])
+        img = cv2.imread(file)
         if not prev:
             prev = curr
         if curr - prev != 1:
             # we're in a different class!
             class_number += 1
-        output_fh.write(class_number)
-        output_fh.write("suhhhh dude")
+        output_fh.write(str(class_number) + "\n")
         prev = curr
-        if counter > 20:
-            break
-        counter += 1
     return
 
 if __name__ == "__main__":
