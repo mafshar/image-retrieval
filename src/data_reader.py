46afd46a8329275c89e80d81e23ab21489ed8c63
Mohammad Afshar <ma2510@nyu.edu>
2016-04-19 22:49:53 -0400

    a way to write the features to a file

--- src/data_reader.py
+++ src/data_reader.py
@@ -8,20 +8,28 @@ import time
 
 INPUT_PATH = "../data/"
 OUTPUT_PATH = "../volume/"
+TYPES_OF_FEATURES = ["raw", "sift", "surf", "flat-histo"]
 
-def read_image():
-    return
-
-def main():
+def create_output_dir(path):
     ## creating the output file if it doesn't exist:
-    if not os.path.exists(os.path.dirname(OUTPUT_PATH)):
+    if not os.path.exists(os.path.dirname(path)):
         try:
-            os.makedirs(os.path.dirname(OUTPUT_PATH))
+            os.makedirs(os.path.dirname(path))
         except OSError as exc: # guard against race condition
             if exc.errno != errno.EEXIST:
                 raise
+    return
+
+def build_features():
+
+    return
+
+
+def read_image(path):
+    create_output_dir(OUTPUT_PATH)
     files = glob.glob(INPUT_PATH + "*.jpg")
-    output_fh = open(OUTPUT_PATH + "labels.txt", "w")
+    output_fh_features = open(OUTPUT_PATH + "features.txt", "w")
+    output_fh_label = open(OUTPUT_PATH + "labels.txt", "w")
     class_number = 0
     prev = None
     curr = None
@@ -35,8 +43,14 @@ def main():
         if curr - prev != 1:
             # we're in a different class!
             class_number += 1
-        output_fh.write(str(class_number) + "\n")
+        output_fh_labels.write(str(class_number) + "\n")
+        build_features()
         prev = curr
+    return
+
+
+def main():
+    read_image(INPUT_PATH)
     return
 
 if __name__ == "__main__":
