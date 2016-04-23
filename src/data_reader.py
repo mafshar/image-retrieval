146ea1ef5b7378d2fc23febcacbfe0a318706f6f
Mohammad Afshar <ma2510@nyu.edu>
2016-04-23 00:12:49 -0400

    structuring framework + adding surf features

--- src/data_reader.py
+++ src/data_reader.py
@@ -6,6 +6,9 @@ import cv2
 import glob
 import os
 import time
+from sklearn.preprocessing import MinMaxScaler
+np.set_printoptions(threshold='nan')
+
 
 INPUT_PATH = "../data/"
 OUTPUT_PATH = "../volume/"
@@ -20,76 +23,80 @@ def create_output_dir(path):
                 raise
     return
 
-# not very useful (will use PCA)
-def build_raw_features(img):
-    return img.flatten()
-
-# also might not be very useful
-def build_color_mean_stdv_features(img):
-    mean, stdv = cv2.meanStdDev(img)
-    return np.concatenate([means, stdv]).flatten()
-
-def build_flat_histo_features(img):
-    return cv2.calcHist([img], [0, 1, 2], None, [8, 8, 8], \
-        [0, 256, 0, 256, 0, 256]).flatten()
-
-def build_sift_features(gray_img, whitening=False):
-    sift = cv2.xfeatures2d.SIFT_create()
-    keypoints, descriptors = sift.detectAndCompute(gray_img, None)
-    descriptors = spvq.whiten(descriptors)
-    return
-
-def build_surf_features():
-    return
-
-def feature_writer(img, output_fh_features):
+def surf_extraction(files, output_fh_features, output_fh_features, \
+        weighting, normalize, counter, vlad=False):
     '''
-    builds the feature vector to be used
-    '''
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # histo_feature = build_flat_histo_features(img)
-    build_sift_features(gray_img)
-    return
 
-## all images are in grayscale
-def main():
-    # create_output_dir(OUTPUT_PATH)
-    files = glob.glob(os.path.join(INPUT_PATH + "*.jpg"))
-    # looping through the images
-    output_fh_features = open(os.path.join(OUTPUT_PATH, "features.txt"), "w")
-    output_fh_labels = open(os.path.join(OUTPUT_PATH, "labels.txt"), "w")
+    '''
     descriptors = []
-    images_and_descriptors = {}
-    counter = 10
+    images_and_descriptors = [] ## matrix of (image_path, descriptors)
     for file in files:
-        # t0 = time.time()
+        t0 = time.time()
         img = cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2GRAY)
         img = cv2.resize(img, (1024, 768))
         sift = cv2.xfeatures2d.SURF_create()
         keypoints, des = sift.detectAndCompute(img, None)
-        images_and_descriptors[file] = des
+        images_and_descriptors.append((file, des))
         for item in des:
             descriptors.append(item)
-        # print time.time() - t0
-        # print des.shape
+        print time.time() - t0
+        print des.shape
         counter -= 1
         if counter == 0:
             break
-    ## whitening helps with kmeans convergence
-    descriptors = spvq.whiten(np.array(descriptors))
+        ## now do k-means clustering:
+        t0 = time.time()
+        k = 1024
+        vocab, variance = spvq.kmeans(descriptors, k, 1)
+        ## vector quantization:
+        data = np.zeros((len(images_and_descriptors), k))
+        for i in range(len(images_and_descriptors)):
+            words, distance = spvq.vq(images_and_descriptors[i][1], vocab)
+            for word in words:
+                data[i][word] += 1
+        if normalize:
+            data = MinMaxScaler().fit_transform(data)
+    return
 
-    ## now do k-means clustering:
-    print "starting clustering"
-    t0 = time.time()
-    k = 1024
-    vocab, variance = spvq.kmeans(descriptors, k, 1)
-    print time.time() - t0
 
-    ## vector quantization:
-    data = []
-    feature = np.zeros(k)
-    # for img_path,
-    #     words, distance = spvq.vq(vocab, file)
+def build_features(feature_type="sift", weighting=False, normalize=False. counter=-1):
+    '''
+    proper documentation here
+    '''
+    files = glob.glob(os.path.join(INPUT_PATH, "*.jpg"))
+    output_fh_features = open(os.path.join(OUTPUT_PATH, "features.txt"), "w")
+    output_fh_labels = open(os.path.join(OUTPUT_PATH, "labels.txt"), "w")
+
+    if feature_type.lower() == "raw":
+        return img.flatten()
+    elif feature_type.lower() == "histo":
+        return cv2.calcHist([img], [0, 1, 2], None, [8, 8, 8], \
+            [0, 256, 0, 256, 0, 256]).flatten()
+    elif feature_type.lower() == "color-mean":
+        mean, stdv = cv2.meanStdDev(img)
+        return np.concatenate([means, stdv]).flatten()
+    elif feature_type.lower() == "sift":
+        print "sifty"
+    elif feature_type.lower() == "sift-vlad":
+        print "sifty-vlad"
+    elif feature_type.lower() == "surf":
+        print "surfy"
+        surf_extraction(files, output_fh_features, output_fh_features, \
+                weighting, normalize, counter, vlad=False)
+    elif feature_type.lower() == "surf-vlad":
+        print "surfy-vlad"
+    elif feature_type.lower() == "alexnet":
+        print "alexnet"
+    else:
+        print "Feature type not recognized"
+
+    return
+
+
+## all images are in grayscale
+def main():
+    # create_output_dir(OUTPUT_PATH)
+
 
 
 
