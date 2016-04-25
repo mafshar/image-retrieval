2edb6f24dcf13d722d81fd2b0390cfec7cc849ae
Mohammad Afshar <ma2510@nyu.edu>
2016-04-24 23:45:26 -0400

    added raw feature extraction

--- src/data_reader.py
+++ src/data_reader.py
@@ -32,18 +32,59 @@ def create_dir(path):
                 raise
     return
 
-# TODO: vlad feature vectors, labels
-def surf_extraction(files, output_path, weighting, normalize, counter, \
-        verbose, vlad=False,):
+def raw_extraction(files, output_path, normalize, counter, verbose):
     '''
-    Function creates a directory in the presently working directory, writes
-    features and labels to that directory.
+    Function builds and writes raw features.
 
     PARAMETERS:
         files: an array of all the images in the directory to extract features
             from
         output_fh_features: file-handle for where to write the features
-        output_fh_labels: file-handle for where to write the labels
+        normalize: bool value to indicate whether the features should be
+            normalized before writing them to disk
+        counter: bool value to indicate whether verbose output is printed to
+            console
+
+    RETURNS:
+        Nothing
+
+    '''
+    create_dir(output_path)
+    print "obtaining the raw features"
+    t0 = time.time()
+    data = []
+    num = 0
+    for file in files:
+        t1 = time.time()
+        img = cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (1024, 768))
+        data.append(img.flatten())
+        if verbose:
+            num += 1
+            print "\traw feature for image", num, "took", time.time() - t1,
+            print "seconds"
+        if counter != -1:
+            if counter == 0:
+                break
+            else:
+                counter -= 1
+    # end for
+    if normalize:
+        data = MinMaxScaler().fit_transform(data)
+    print "writing to file..."
+    np.save(os.path.join(output_path, "raw_features"), data)
+    return
+
+# TODO: vlad feature vectors, labels
+def surf_extraction(files, output_path, weighting, normalize, counter, \
+        verbose, vlad=False,):
+    '''
+    Function builds and writes SURF features.
+
+    PARAMETERS:
+        files: an array of all the images in the directory to extract features
+            from
+        output_fh_features: file-handle for where to write the features
         weighting: bool value to indicate whether tf-idf weighting should be
             done to the features before writing them to disk
         normalize: bool value to indicate whether the features should be
@@ -56,9 +97,9 @@ def surf_extraction(files, output_path, weighting, normalize, counter, \
     RETURNS:
         Nothing
     '''
+    create_dir(output_path)
     descriptors = []
     images_and_descriptors = [] ## matrix of (image_path, descriptors)
-    create_dir(output_path)
     print "obtaining the descriptors"
     t0 = time.time()
     num = 0
@@ -100,7 +141,11 @@ def surf_extraction(files, output_path, weighting, normalize, counter, \
     print "vector quantization took", time.time() - t3, "seconds"
     if normalize:
         data = MinMaxScaler().fit_transform(data)
-    np.savez("surf", data)
+    print "writing to file..."
+    if vlad:
+        np.save(os.path.join(output_path, "vlad_surf_features"), data)
+    else:
+        np.save(os.path.join(output_path, "surf_features"), data)
     return
 
 
@@ -111,21 +156,21 @@ def build_features(input_path=INPUT_PATH, output_path=OUTPUT_PATH, \
     '''
     files = glob.glob(os.path.join(input_path, "*.jpg"))
     if feature_type.lower() == "raw":
-        return img.flatten()
+        raw_extraction(files, output_path, normalize, counter, verbose)
     elif feature_type.lower() == "histo":
         return cv2.calcHist([img], [0, 1, 2], None, [8, 8, 8], \
             [0, 256, 0, 256, 0, 256]).flatten()
-    elif feature_type.lower() == "color-mean":
+    elif feature_type.lower() == "color_mean":
         mean, stdv = cv2.meanStdDev(img)
         return np.concatenate([means, stdv]).flatten()
     elif feature_type.lower() == "sift":
         print "sifty"
-    elif feature_type.lower() == "sift-vlad":
+    elif feature_type.lower() == "vlad_sift":
         print "sifty-vlad"
     elif feature_type.lower() == "surf":
         surf_extraction(files, output_path, weighting, normalize, counter, \
             verbose, vlad=False,)
-    elif feature_type.lower() == "surf-vlad":
+    elif feature_type.lower() == "vlad_surf":
         print "surfy-vlad"
     elif feature_type.lower() == "alexnet":
         print "alexnet"
@@ -137,9 +182,8 @@ def build_features(input_path=INPUT_PATH, output_path=OUTPUT_PATH, \
 ## all images are in grayscale
 def main():
     create_dir(OUTPUT_PATH) # creates the top-level output directory
-    build_features(output_path=os.path.join(OUTPUT_PATH, "surf"), \
-        feature_type="surf", counter=10, verbose=True)
-
+    build_features(output_path=OUTPUT_PATH, feature_type="raw", counter=10,\
+        verbose=True)
 
     return
 
