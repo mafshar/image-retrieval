072464b147ac547612ee86c15fefbd935cff2c5b
Mohammad Afshar <ma2510@nyu.edu>
2016-04-25 00:04:44 -0400

    adding sift feature extraction

--- src/data_reader.py
+++ src/data_reader.py
@@ -82,6 +82,79 @@ def raw_extraction(files, output_path, normalize, dim_reduction, \
     return
 
 # TODO: vlad feature vectors, labels
+def sift_extraction(files, output_path, weighting, normalize, counter, \
+        verbose, vlad=False,):
+    '''
+    Function builds and writes SIFT features.
+
+    PARAMETERS:
+        files: an array of all the images in the directory to extract features
+            from
+        output_fh_features: file-handle for where to write the features
+        weighting: bool value to indicate whether tf-idf weighting should be
+            done to the features before writing them to disk
+        normalize: bool value to indicate whether the features should be
+            normalized before writing them to disk
+        counter: bool value to indicate whether verbose output is printed to
+            console
+        vlad: bool value to indicate whether it bag-of-visual words
+            representation should be used or VLAD feature vectors
+
+    RETURNS:
+        Nothing
+    '''
+    create_dir(output_path)
+    descriptors = []
+    images_and_descriptors = [] ## matrix of (image_path, descriptors)
+    print "obtaining the descriptors"
+    t0 = time.time()
+    num = 0
+    for file in files:
+        t1 = time.time()
+        img = cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (1024, 768))
+        sift = cv2.xfeatures2d.SIFT_create()
+        keypoints, des = sift.detectAndCompute(img, None)
+        images_and_descriptors.append((file, des))
+        for item in des:
+            descriptors.append(item)
+        if verbose:
+            num += 1
+            print "\tSIFT descriptors for image", num, "took", time.time() - t1,
+            print "seconds"
+        if counter != -1:
+            if counter == 0:
+                break
+            else:
+                counter -= 1
+    # end for
+    print "getting all the descriptors took", time.time() - t0, "seconds"
+    ## now do k-means clustering:
+    print "starting kmeans clustering"
+    t2 = time.time()
+    k = 1024
+    vocab, variance = spvq.kmeans(descriptors, k, 1)
+    print "kmeans took", time.time() - t2, "seconds"
+    ## vector quantization:
+    print "starting vector quantization"
+    t3 = time.time()
+    data = np.zeros((len(images_and_descriptors), k))
+    for i in range(len(images_and_descriptors)):
+        words, distance = spvq.vq(images_and_descriptors[i][1], vocab)
+        for word in words:
+            data[i][word] += 1
+    # end for
+    print "vector quantization took", time.time() - t3, "seconds"
+    if normalize:
+        data = MinMaxScaler().fit_transform(data)
+    print "writing to file..."
+    if vlad:
+        np.save(os.path.join(output_path, "vlad_sift_features"), data)
+    else:
+        np.save(os.path.join(output_path, "sift_features"), data)
+    return
+
+# TODO: vlad feature vectors, labels
 def surf_extraction(files, output_path, weighting, normalize, counter, \
         verbose, vlad=False,):
     '''
@@ -120,7 +193,7 @@ def surf_extraction(files, output_path, weighting, normalize, counter, \
             descriptors.append(item)
         if verbose:
             num += 1
-            print "\tdescriptor for image", num, "took", time.time() - t1,
+            print "\tSURF descriptors for image", num, "took", time.time() - t1,
             print "seconds"
         if counter != -1:
             if counter == 0:
@@ -172,27 +245,32 @@ def build_features(input_path=INPUT_PATH, output_path=OUTPUT_PATH, \
         mean, stdv = cv2.meanStdDev(img)
         return np.concatenate([means, stdv]).flatten()
     elif feature_type.lower() == "sift":
-        print "sifty"
+        sift_extraction(files, output_path, weighting, normalize, counter, \
+            verbose, vlad=False)
     elif feature_type.lower() == "vlad_sift":
-        print "sifty-vlad"
+        sift_extraction(files, output_path, weighting, normalize, counter, \
+            verbose, vlad=True)
     elif feature_type.lower() == "surf":
         surf_extraction(files, output_path, weighting, normalize, counter, \
-            verbose, vlad=False,)
+            verbose, vlad=False)
     elif feature_type.lower() == "vlad_surf":
-        print "surfy-vlad"
+        surf_extraction(files, output_path, weighting, normalize, counter, \
+            verbose, vlad=True)
     elif feature_type.lower() == "alexnet":
         print "alexnet"
     else:
         print "Feature type not recognized"
-
     return
 
 ## all images are in grayscale
 def main():
     create_dir(OUTPUT_PATH) # creates the top-level output directory
-    build_features(output_path=OUTPUT_PATH, feature_type="raw", counter=10,\
-        verbose=True)
-
+    # build_features(output_path=OUTPUT_PATH, feature_type="raw", counter=10,\
+    #     verbose=True)
+    # build_features(output_path=OUTPUT_PATH, feature_type="sift", counter=10,\
+    #     verbose=True)
+    # build_features(output_path=OUTPUT_PATH, feature_type="surf", counter=10,\
+        # verbose=True)
 
     return
 
