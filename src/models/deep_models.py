abe6d6d2813050639dfd33539cd9c656dd26b3b4
mafshar <ma2510@nyu.edu>
2018-07-23 10:00:46 -0400

    refactored model architecture for cleaner implementation

--- src/models/deep_models.py
+++ src/models/deep_models.py
@@ -16,49 +16,58 @@ class ConvolutionalDenoisingAutoencoder(nn.Module):
         super(
             ConvolutionalDenoisingAutoencoder,
             self).__init__()
-        # ENCODER
-        self.conv1 = nn.Conv2d(3, 8, 3)
-        self.conv2 = nn.Conv2d(8, 16, 3)
-        self.conv3 = nn.Conv2d(16, 16, 3)
-        # DECODER
-        self.conv4 = nn.Conv2d(16, 32, 3)
-        self.upsample1 = nn.Upsample( # equivalent of upsample2d
-            size=(2, 2),
-            mode='bilinear',
-            align_corners=True)
-        self.conv5 = nn.Conv2d(32, 32, 3)
-        self.upsample2 = nn.Upsample( # equivalent of upsample2d
-            size=(2, 2),
-            mode='bilinear',
-            align_corners=True)
-        self.conv6 = nn.Conv2d(32, 16, 3)
-        self.upsample3 = nn.Upsample( # equivalent of upsample2d
-            size=(2, 2),
-            mode='bilinear',
-            align_corners=True)
-        self.conv7 = nn.Conv2d(16, 16, 3)
-        self.upsample4 = nn.Upsample( # equivalent of upsample2d
-            size=(2, 2),
-            mode='bilinear',
-            align_corners=True)
-        self.conv8 = nn.Conv2d(16, 8, 3)
-        self.upsample5 = nn.Upsample( # equivalent of upsample2d
-            size=(2, 2),
-            mode='bilinear',
-            align_corners=True)
-        self.conv9 = nn.Conv2d(8, 3, 3)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 8, 3),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(8, 16, 3),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(16, 16, 3),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2)
+        )
+        self.decoder = nn.Sequential(
+            nn.Conv2d(16, 32, 3),
+            nn.ReLU(True)
+            nn.Upsample(
+                size=(2, 2),
+                mode='bilinear',
+                align_corners=True
+            ),
+            nn.Conv2d(32, 32, 3),
+            nn.ReLU(True)
+            nn.Upsample(
+                size=(2, 2),
+                mode='bilinear',
+                align_corners=True
+            ),
+            nn.Conv2d(32, 16, 3),
+            nn.ReLU(True)
+            nn.Upsample(
+                size=(2, 2),
+                mode='bilinear',
+                align_corners=True
+            ),
+            nn.Conv2d(16, 16, 3),
+            nn.ReLU(True)
+            nn.Upsample(
+                size=(2, 2),
+                mode='bilinear',
+                align_corners=True
+            ),
+            nn.Conv2d(16, 8, 3),
+            nn.ReLU(True)
+            nn.Upsample(
+                size=(2, 2),
+                mode='bilinear',
+                align_corners=True
+            ),
+            nn.Conv2d(8, 3, 3)
+        )
         return
 
     def forward(self, x):
-        ## ENCODER
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
-        encoded = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))
-        ## DECODER
-        x = self.upsample1(F.relu(self.conv4(x)))
-        x = self.upsample2(F.relu(self.conv5(x)))
-        x = self.upsample3(F.relu(self.conv6(x)))
-        x = self.upsample4(F.relu(self.conv7(x)))
-        x = self.upsample5(F.relu(self.conv8(x)))
-        decoded = self.conv9(x)
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
         return decoded
