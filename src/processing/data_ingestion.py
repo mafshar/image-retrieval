13775c8c70f14274c09fe3ce7e66e2d7a72ff6a2
mafshar <ma2510@nyu.edu>
2018-07-23 09:23:21 -0400

    major bug fix in data loading and Dataset class

--- src/processing/data_ingestion.py
+++ src/processing/data_ingestion.py
@@ -8,22 +8,26 @@ warnings.filterwarnings('ignore')
 
 import numpy as np
 import pandas as pd
+import torch
 
 from os.path import join
 from os import listdir
 from scipy.misc import imread, imresize, imsave
+from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
+
 from torchvision import transforms
+
 
 DATA_DIR = './data'
 IMG_DIR = join(DATA_DIR, 'jpg')
-IMG_SIZE = 299
+IMG_SIZE = 256
 
 
 def __get_transformations():
     return transforms.Compose(
-        [transforms.Resize(IMG_SIZE),
+        [transforms.Resize((IMG_SIZE, IMG_SIZE)),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
@@ -52,16 +56,16 @@ def __create_data_loader(dataset, batch_size, num_workers=0):
 class HolidayDataset(Dataset):
 
     def __init__(self, dir_path=DATA_DIR, transform=None):
-        self.files = [f for f in listdir(dir_path) if f.endswith('jpg')]
+        self.files = [join(dir_path, f) for f in listdir(dir_path) if f.endswith('jpg')]
         self.transform = transform
         return
 
     def __len__(self):
         return len(self.files)
 
-    def __get__item(self, idx):
+    def __getitem__(self, idx):
         filename = self.files[idx]
-        img = imread(filename, mode='L')
+        img = Image.open(filename)
         if self.transform:
             img = self.transform(img)
         return img
